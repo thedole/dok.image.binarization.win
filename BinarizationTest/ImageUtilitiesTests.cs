de5143b0d11346969075b9ee54ff299405988d11
Dag Ole Kristoffersen <thedole@gmail.com>
2016-04-04 17:44:10 +0200

    Added test for ReadImageFolder

--- BinarizationTest/ImageUtilitiesTests.cs
+++ BinarizationTest/ImageUtilitiesTests.cs
@@ -1,5 +1,7 @@
-﻿using Emgu.CV;
+﻿using dok.image.binarization.win;
+using Emgu.CV;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -8,10 +10,11 @@ namespace dok.image.binarization.win.Tests
     [TestClass()]
     public class ImageUtilitiesTests
     {
+        private static readonly string testImagesFolder = $".{Path.DirectorySeparatorChar}testImages";
         [TestMethod()]
         public void ReadImageTest()
         {
-            var testImageName = Directory.EnumerateFileSystemEntries($".{Path.DirectorySeparatorChar}testImages").FirstOrDefault();
+            var testImageName = Directory.EnumerateFileSystemEntries(testImagesFolder).FirstOrDefault();
             var image = ImageUtilities.ReadImage(testImageName);
             Assert.IsInstanceOfType(image, typeof(Mat));
         }
@@ -21,6 +24,14 @@ namespace dok.image.binarization.win.Tests
         public void True()
         {
             Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void ReadImageFolderTest()
+        {
+            var images = ImageUtilities.ReadImageFolder(testImagesFolder);
+            Assert.IsInstanceOfType(images, typeof(IEnumerable<Mat>));
+            Assert.AreNotEqual(0, images.Count(), "No images read from testImagesFolder");
         }
     }
 }
