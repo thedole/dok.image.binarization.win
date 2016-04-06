1c83c61d955b3ac68eb3ae9d7682f1664751c429
Dag Ole Kristoffersen <thedole@gmail.com>
2016-04-07 00:58:30 +0200

    Added functions for binarization of an image using sauvolas method

--- dok.image.binarization.win/ImageUtilities.cs
+++ dok.image.binarization.win/ImageUtilities.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using OpenCvSharp.CPlusPlus;
 using OpenCvSharp;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.Windows.Interop;
+using System.Windows;
+using OpenCvSharp.Extensions;
 
 namespace dok.image.binarization.win
 {
-    public class ImageUtilities
+    public static class ImageUtilities
     {
         public static Mat ReadImage(string path, LoadMode loadMode = LoadMode.AnyColor)
         {
@@ -73,6 +77,92 @@ namespace dok.image.binarization.win
                 }
             }
             return false;
+        }
+
+        public static Bitmap ToBitmap(this BitmapImage bitmapImage)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                var enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                var bitmap = new Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+        public static BitmapSource ToBitmapImage(this Bitmap bitmap)
+        {
+            BitmapSource i = Imaging.CreateBitmapSourceFromHBitmap(
+                           bitmap.GetHbitmap(),
+                           IntPtr.Zero,
+                           Int32Rect.Empty,
+                           BitmapSizeOptions.FromEmptyOptions());
+            return i;
+        }
+
+        public static MeanStdDev MeanStdDev(this Mat source)
+        {
+            var mean = new Scalar();
+            var stdDev = new Scalar(); ;
+
+            Cv2.MeanStdDev(source, out mean, out stdDev);
+
+            return new MeanStdDev
+            {
+                Mean = mean,
+                StdDev = stdDev
+            };
+        }
+
+        public static int IndexOfBiggestStdDev(this MeanStdDev meanStdDev)
+        {
+            var stdDev = meanStdDev.StdDev;
+            var deviations = new[] { stdDev.Val0, stdDev.Val1, stdDev.Val2, stdDev.Val3 };
+            var biggestIndex = 0;
+            for (int i = 1; i < deviations.Length; i++)
+            {
+                if (deviations[i] < deviations[biggestIndex])
+                {
+                    continue;
+                }
+
+                biggestIndex = i;
+            }
+
+            return biggestIndex;
+        }
+        
+        private static IplImage Binarize(this IplImage sourceIplImage, int size = 51, double k = 0.2, double r = 128)
+        {
+            var destinationIplImage = sourceIplImage.EmptyClone();
+            Binarizer.SauvolaFast(sourceIplImage, destinationIplImage, size, k, r);
+            return destinationIplImage;
+        }
+
+        public static Bitmap Binarize(this BitmapImage sourceBitmap, int size = 200, double k = 0.2, double r = 128)
+        {
+            var sourceMat = sourceBitmap.ToMat();
+            var meanStdDev = sourceMat.MeanStdDev();
+            var channelIndex = meanStdDev.IndexOfBiggestStdDev();
+            var channel = sourceMat.Split()[channelIndex];
+            var channelMean = meanStdDev[channelIndex].Item1;
+            var actualR = channelMean * 0.3;
+
+            var sourceIplImage = channel
+                            .ToIplImage();
+            IplImage destinationIplImage = sourceIplImage.Binarize(size: 51, k: 0.2, r: actualR);
+
+            var outputBitmap = destinationIplImage.ToBitmap();
+            return outputBitmap;
+        }
+
+        public static void SaveToDisk(this BitmapSource bitmapSource, string fileName = "image.png")
+        {
+            bitmapSource
+                .ToMat()
+                .SaveImage(fileName);
         }
     }
 }
---
+++ dok.image.binarization.win/MeanStdDev.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using OpenCvSharp.CPlusPlus;
+
+namespace dok.image.binarization.win
+{
+    public class MeanStdDev
+    {
+        public Scalar Mean { get; set; }
+        public Scalar StdDev { get; set; }
+
+        public Tuple<double, double> this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                {
+                    return Tuple.Create(Mean.Val0, StdDev.Val0);
+                }
+                if (index == 1)
+                {
+                    return Tuple.Create(Mean.Val1, StdDev.Val1);
+                }
+                if (index == 2)
+                {
+                    return Tuple.Create(Mean.Val2, StdDev.Val2);
+                }
+                if (index == 3)
+                {
+                    return Tuple.Create(Mean.Val3, StdDev.Val3);
+                }
+
+                return null;
+            }
+        }
+    }
+}
